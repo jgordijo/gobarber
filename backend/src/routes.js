19583f877a7081a937ad8de61bdbed78c4de5476
Julio <jgordijo@gmail.com>
2020-02-21 16:01:23 -0300

    Schedule do provider e instalacao do mongoose

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import SchedulleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,6 +24,8 @@ routes.get('/providers', ProviderController.index);
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/schedule', SchedulleController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
