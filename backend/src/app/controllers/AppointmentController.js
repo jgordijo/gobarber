729b856ad6468d4ffc47c8d9cdff1ddc5e0c72c3
Julio <jgordijo@gmail.com>
2020-02-21 11:39:30 -0300

    Validacao dos appointments

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointments';
 
@@ -31,10 +32,44 @@ class AppointmentController {
             });
         }
 
+        /*
+         *
+         * Check for past dates
+         *
+         */
+
+        const hourStart = startOfHour(parseISO(date));
+
+        if (isBefore(hourStart, new Date())) {
+            return res
+                .status(400)
+                .json({ error: 'Past dates are not allowed' });
+        }
+
+        /*
+         *
+         * Check date availability
+         *
+         */
+
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if (checkAvailability) {
+            return res
+                .status(400)
+                .json({ error: 'Appointment date is not available' });
+        }
+
         const appointment = await Appointment.create({
             user_id: req.userId,
             provider_id,
-            date,
+            date: hourStart,
         });
 
         return res.json(appointment);
